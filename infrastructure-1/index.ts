7b50a27ec172bfc016392e3d566881bf4e828c1e
AliZgheib <zgheibali@gmail.com>
2025-04-06 17:34:48 +0300

    Update infra-1 and infra-2

--- infrastructure-1/index.ts
+++ infrastructure-1/index.ts
@@ -1,27 +1,31 @@
 import * as aws from "@pulumi/aws";
 import * as synced from "@pulumi/synced-folder";
 
+// Create an S3 bucket configured for static website hosting
 const bucket = new aws.s3.Bucket("dev-mood-1-bucket", {
   website: {
-    indexDocument: "index.html", // Configure index.html as the entry point
-    errorDocument: "404.html", // Optionally set up error pages
+    indexDocument: "index.html", // Main entry point of the static site
+    errorDocument: "404.html",   // Optional custom error page
   },
 });
 
+// Upload contents of the `./out` directory to the S3 bucket
 new synced.S3BucketFolder("dev-mood-1-bucket-folder", {
-  path: "./out",
-  bucketName: bucket.bucket,
-  acl: aws.s3.PrivateAcl,
+  path: "./out",              // Local path to the static build output
+  bucketName: bucket.bucket,  // Reference to the S3 bucket name
+  acl: aws.s3.PrivateAcl,     // Files are uploaded with private ACL
 });
 
+// Allow public access by disabling all S3 Public Access Blocks
 new aws.s3.BucketPublicAccessBlock("dev-mood-1-bucket-public-access-block", {
   bucket: bucket.id,
-  blockPublicAcls: false,
-  blockPublicPolicy: false,
-  ignorePublicAcls: false,
-  restrictPublicBuckets: false,
+  blockPublicAcls: false,        // Allow use of public ACLs
+  blockPublicPolicy: false,      // Allow public bucket policies
+  ignorePublicAcls: false,       // Do not ignore public ACLs
+  restrictPublicBuckets: false,  // Do not restrict public access
 });
 
+// Create a bucket policy that allows public read access to all files
 new aws.s3.BucketPolicy("dev-mood-1-bucket-policy", {
   bucket: bucket.id,
   policy: bucket.bucket.apply((bucketName) =>
@@ -29,17 +33,18 @@ new aws.s3.BucketPolicy("dev-mood-1-bucket-policy", {
       Version: "2012-10-17",
       Statement: [
         {
-          Sid: "PublicReadGetObject",
-          Effect: "Allow",
-          Principal: "*",
-          Action: "s3:GetObject",
-          Resource: `arn:aws:s3:::${bucketName}/*`,
+          Sid: "PublicReadGetObject", // Statement identifier
+          Effect: "Allow",            // Allow access
+          Principal: "*",             // Any user (public)
+          Action: "s3:GetObject",     // Allow read-only access
+          Resource: `arn:aws:s3:::${bucketName}/*`, // Target all objects in the bucket
         },
       ],
     })
   ),
 });
 
+// Export the website's public URL (HTTP only)
 export const websiteUrl = bucket.websiteEndpoint.apply(
   (websiteEndpoint) => `http://${websiteEndpoint}`
 );
--- infrastructure-2/index.ts
+++ infrastructure-2/index.ts
@@ -1,9 +1,91 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import * as pulumi from "@pulumi/pulumi";
+import * as synced from "@pulumi/synced-folder";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.BucketV2("my-bucket");
+// Create private S3 bucket
+const bucket = new aws.s3.Bucket("dev-mood-2-bucket");
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+// Sync local ./out directory to S3
+new synced.S3BucketFolder("dev-mood-2-bucket-folder", {
+  path: "./out",
+  bucketName: bucket.bucket,
+  acl: aws.s3.PrivateAcl,
+});
+
+// Origin Access Control (OAC) for CloudFront -> S3 access
+const originAccessControl = new aws.cloudfront.OriginAccessControl("dev-mood-2-oac", {
+  description: "OAC for dev-mood-2 CloudFront access to S3",
+  originAccessControlOriginType: "s3",
+  signingBehavior: "always",
+  signingProtocol: "sigv4",
+});
+
+// CloudFront Distribution
+const distribution = new aws.cloudfront.Distribution("dev-mood-2-cdn", {
+  enabled: true,
+  origins: [
+    {
+      domainName: bucket.bucketRegionalDomainName,
+      originId: bucket.arn,
+      originAccessControlId: originAccessControl.id,
+    },
+  ],
+  defaultRootObject: "index.html",
+  defaultCacheBehavior: {
+    targetOriginId: bucket.arn,
+    viewerProtocolPolicy: "redirect-to-https",
+    allowedMethods: ["GET", "HEAD"],
+    cachedMethods: ["GET", "HEAD"],
+    forwardedValues: {
+      queryString: false,
+      cookies: { forward: "none" },
+    },
+  },
+  priceClass: "PriceClass_100",
+  viewerCertificate: {
+    cloudfrontDefaultCertificate: true,
+  },
+  restrictions: {
+    geoRestriction: {
+      restrictionType: "none",
+    },
+  },
+});
+
+// Public access is disabled, only CloudFront can read
+new aws.s3.BucketPublicAccessBlock("dev-mood-2-bucket-public-access-block", {
+  bucket: bucket.id,
+  blockPublicAcls: true,
+  blockPublicPolicy: true,
+  ignorePublicAcls: true,
+  restrictPublicBuckets: true,
+});
+
+// Bucket policy to allow CloudFront (via OAC) to access objects
+new aws.s3.BucketPolicy("dev-mood-2-bucket-policy", {
+  bucket: bucket.id,
+  policy: pulumi.all([bucket.arn, distribution.arn]).apply(([bucketArn, cfArn]) =>
+    JSON.stringify({
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Sid: "AllowCloudFrontServicePrincipalReadOnly",
+          Effect: "Allow",
+          Principal: {
+            Service: "cloudfront.amazonaws.com",
+          },
+          Action: "s3:GetObject",
+          Resource: `${bucketArn}/*`,
+          Condition: {
+            StringEquals: {
+              "AWS:SourceArn": cfArn,
+            },
+          },
+        },
+      ],
+    })
+  ),
+});
+
+// Export CDN URL
+export const websiteUrl = pulumi.interpolate`https://${distribution.domainName}`;
